4552b4a57014c63fca82532be2440366c58d7001
DmytroKaniuchok <kanyuchekdmitry@gmail.com>
2025-02-13 23:17:20 +0200

    fixed errors

--- js/task-1.js
+++ js/task-1.js
@@ -2,8 +2,8 @@ const itemsList = document.querySelectorAll("li.item");
 console.log(`Number of categories: ${itemsList.length}`);
 
 itemsList.forEach((item) => {
-  const itemTitle = querySelector("h2");
-  const itemsElements = querySelectorAll("ul li");
+  const itemTitle = item.querySelector("h2");
+  const itemsElements = item.querySelectorAll("ul li");
   console.log(`Category: ${itemTitle.textContent}`);
   console.log(`Elements: ${itemsElements.length}`);
 });
